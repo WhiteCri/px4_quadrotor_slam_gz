ac1658a84f56d0f78ca4ef1efd40273edadddb42
Taewook Park <sjrnfu12@naver.com>
2021-05-06 14:54:08 +0900

    add px4 open_vins support

--- px4_gazebo_sensor_openvins_integration.sh
+++ px4_gazebo_sensor_openvins_integration.sh
@@ -12,10 +12,10 @@
 world="warehouse"
 vehicle="iris_rplidar"
 
-cd ~/src/Firmware
+cd ~/src/PX4-Autopilot
 DONT_RUN=1 make px4_sitl gazebo_${vehicle}__${world}
 source ~/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-roslaunch px4_quad_slam_simulation px4_sensor_openvins_integration.launch fcu_url:="udp://:14540@127.0.0.1:14557"
+roslaunch px4_quad_slam_simulation px4_sensor_openvins_integration.launch 
